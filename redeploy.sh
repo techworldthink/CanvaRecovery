600df2c84873960809ce58ccc9bc1bd09d9aea43
techworldthink <jobin25051999@gmail.com>
2026-01-20 09:03:04 +0530

    execute

--- redeploy.sh
+++ redeploy.sh
@@ -11,5 +11,5 @@ docker build -t canva-recovery .
 
 # Run the new container
 docker run -d -p 8084:8084 -v $(pwd)/data:/app/data --name canva-recovery canva-recovery
-
+ 
 echo "Redeployment successful. Application updated and running on port 8084."
